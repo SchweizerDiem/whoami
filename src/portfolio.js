e8aa482ccfa10370d1d0d9ac56813e71913f70c2
SchweizerDiem <gabrielrochaxtz@gmail.com>
2025-10-19 23:15:13 +0100

    refactor(data): changing data

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,21 +1,21 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://SchweizerDiem.github.io/Portfolio',
+  title: 'GR.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
-  picture: 'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png',
+  name: 'Gabriel Rocha',
+  role: 'Computer Science and Engineering Student',
+  picture: 'profile.png',
 
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    "I am passionate about creating new solutions and colaborating with as many people as I can. I'm currently studying Computer Science and Engineering @ ISPGAYA (Porto, Portugal) and participating in a few BIP's (Blended Intensive Program) ᕙ(`▿´)ᕗ, tackling different problems with people from diverse fields.",
+  resume: 'resume.pdf',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://linkedin.com/in/gabriel-rocha-1b93a0348/',
+    github: 'https://github.com/SchweizerDiem',
   },
 }
 
@@ -23,30 +23,36 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Url Shortener',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-    image: 'cleanfolio.png',
+      'A full-stack web application designed to shorten URL. As well as manage users that use the application',
+    stack: ['Java', 'PostgreSQL', 'SpringBoot'],
+    sourceCode: 'https://github.com/url-shortener',
+    image: 'car-vehicle.png',
   },
   {
-    name: 'Project 2',
+    name: 'Predictive model through intrusion detection system in computer networks',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      '(Coming) COIL project made in colaboration with the Brazillian student team from Fatec Ipiranga',
+    stack: [' '],
     sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-    image: 'https://github.githubassets.com/assets/GitHub-Logo-ee398b662d42.png',
+    image: 'predict.png',
   },
   {
-    name: 'Project 3',
+    name: 'Multi Agent Traffic System',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'A Multi Agent system that simulates traffic, control traffic lights in order to minimize waiting time according to traffic conditions and sudden disaster',
+    stack: ['Python', 'Spade', 'Sumo'],
     sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    image: 'agents.png'
+  },
+  {
+    name: 'Independent Heart',
+    description:
+      "A drag n' drop video game designed to teach children about sustainable energy (҂◡̀_◡́)ᕤ  BIP Project",
+    stack: ['GML'],
+    sourceCode: 'https://github.com',
+    // image: 'agents.png'
   },
 ]
 
@@ -55,21 +61,23 @@ const skills = [
   // if there are no skills, Skills section won't show up
   'HTML',
   'CSS',
-  'JavaScript',
-  'TypeScript',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Github',
+  'Java',
+  'Python',
+  'PostgreSQL',
+  'C#',
+  'PHP',
+  'SQL',
+  'React',
+  '.NET',
+  'Maven',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'gabriel.jdrocha@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
